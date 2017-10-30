a90c6b3c85ea03c7b79180232f13ad5a1c3cf898
Rinat Kashapov <rinatkzz@yandex.ru>
2017-10-30 11:35:57 +0300

    add new features

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -3,7 +3,10 @@
 const addQuestion = document.getElementById('addQuestion');
 const typeQuestion = document.getElementById('typeQuestion');
 const questionsBlock = document.getElementById('questions');
+const saveQuestionary = document.getElementById('saveQuestionary');
 
+
+let arrayData = [];
 let numberQuestion = 1;
 const types = {
     'one': 'Одинарный выбор',
@@ -35,17 +38,24 @@ function clickFunc(event) {
     if (event.target.classList.contains('add-variant')) {
         addVariant(event.target);
     }
-    
+
     if (event.target.classList.contains('delete-question')) {
-        deleteVariant(event.target);
+        deleteQuestion(event.target);
     }
-    
+
     if (event.target.classList.contains('question-move-up')) {
         moveQuestionUp(event.target.parentNode);
     }
     if (event.target.classList.contains('question-move-down')) {
         moveQuestionDown(event.target.parentNode);
     }
+    if (event.target.classList.contains('button-hide')) {
+        compactQuestionBlock(event.target);
+    }
+    if (event.target === saveQuestionary) {
+        saveQuestionaryFunc();
+    }
+
 }
 function addQuestionFunc() {
     const type = typeQuestion.value;
@@ -56,18 +66,23 @@ function addQuestionFunc() {
 
     const divUp = createEl('div', 'question-move-up');
     divWrapper.appendChild(divUp);
-    
-    const divType = createEl('div', 'col-xs-12');
+
+    const divType = createEl('div', 'col-xs-8 col-md-10');
     divType.innerText = types[type];
+    const divHide = createEl('div', 'col-xs-4 col-md-2 text-right');
+    const buttonHide = createEl('button', 'btn btn-default btn-sm button-hide');
+    buttonHide.innerText = '-';
+    divHide.appendChild(buttonHide);
     divWrapper.appendChild(divType);
+    divWrapper.appendChild(divHide);
     let divInput = createEl('div', 'input-group input-block');
-    let inputNameQuestion = createInput('questionName' + numberQuestion, 'form-control');
+    let inputNameQuestion = createInput('questionName' + numberQuestion, 'form-control question-text');
     inputNameQuestion.setAttribute('placeholder', 'Текст вопроса');
     let span = createEl('span', 'input-group-btn');
     const buttonDelete = createEl('button', 'btn btn-danger delete-question');
     buttonDelete.innerText = 'Удалить';
     span.appendChild(buttonDelete);
-    
+
     divInput.appendChild(inputNameQuestion);
     divInput.appendChild(span);
     divWrapper.appendChild(divInput);
@@ -105,7 +120,7 @@ function addVariant(elem) {
     parent.insertBefore(newVariant, elem);
 }
 
-function deleteVariant(elem) {
+function deleteQuestion(elem) {
     const questionBlock = getParentByClassname(elem, 'question-block');
     if (questionBlock) {
         questionsBlock.removeChild(questionBlock);
@@ -134,7 +149,7 @@ function init() {
 }
 
 function fillVariantsType() {
-    
+
     for (let index in types) {
         const option = document.createElement('option');
         option.setAttribute('value', index);
@@ -154,7 +169,7 @@ function moveQuestion(node, up) {
     const upperChildren = node.previousSibling;
     const lowerChildren = node.nextSibling;
     const copyNode = node.cloneNode(true);
-    
+
     if (!up) {
         if (!lowerChildren) {
             return false;
@@ -171,4 +186,61 @@ function moveQuestion(node, up) {
         questionsBlock.insertBefore(copyNode, upperChildren);
     }
     questionsBlock.removeChild(node);
+}
+
+function saveQuestionaryFunc() {
+  arrayData = [];
+  const inputs = questionsBlock.querySelectorAll('input');
+
+  Array.from(inputs).forEach(function(value, index) {
+    if (value.classList.contains('question-text')) {
+      const parent = getParentByClassname(value, 'question-block');
+      if (!parent) {
+        return false;
+      }
+      const type = parent.dataset.type;
+      const questionId = parent.dataset.id;
+      arrayData.push({id: questionId, type: type, text: value.value});
+    } else {
+      let text = value.value;
+      let q_id = value.name.split('_')[0];
+      let row = getRowById(q_id);
+      if (!row.hasOwnProperty('variants')) {
+        row['variants'] = [];
+      }
+      row.variants.push(text);
+    }
+    //console.log(value);
+  });
+  console.log(arrayData);
+}
+
+function getRowById(id) {
+  for(let row of arrayData) {
+    if (row.hasOwnProperty('id') && row.id === id) {
+      return row;
+    }
+  }
+  return null;
+}
+function compactQuestionBlock(elem) {
+  let element = null;
+  if (elem.classList.contains('question-block')) {
+    element = elem;
+  }
+  if (!element) {
+    element = getParentByClassname(elem, 'question-block');
+  }
+  if (!element) {
+    return false;
+  }
+  const buttonHide = element.querySelector('.button-hide');
+
+  if(element.classList.contains('compact')) {
+    element.classList.remove('compact');
+    buttonHide.innerText = '-';
+  } else {
+    element.classList.add('compact');
+    buttonHide.innerText = '+';
+  }
 }
